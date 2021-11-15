8fb31f10d99563a560e9716664573598e3e2ad00
rkoschel <rudolf@koschel.org>
2021-11-15 17:02:21 +0100

    install python libs

--- pi-setup/setup.sh
+++ pi-setup/setup.sh
@@ -1,4 +1,7 @@
 
+sudo pip install flask
+sudo pip install beautifulsoup4
+
 # setup and start bot clock service
 sudo cp ~pi/soccer-club-ranking/pi-setup/scr.service /lib/systemd/system
 sudo chmod 644 /lib/systemd/system/scr.service
