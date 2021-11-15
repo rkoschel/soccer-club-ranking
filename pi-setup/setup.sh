2aa6f1319ee0bee336c8f72e14de0bbea3811837
rkoschel <rudolf@koschel.org>
2021-11-15 16:12:08 +0100

    correct directory name

--- pi-setup/setup.sh
+++ pi-setup/setup.sh
@@ -1,6 +1,6 @@
 
 # setup and start bot clock service
-sudo cp ~pi/soccer-club-ranking/setup/scr.service /lib/systemd/system
+sudo cp ~pi/soccer-club-ranking/pi-setup/scr.service /lib/systemd/system
 sudo chmod 644 /lib/systemd/system/scr.service
 sudo systemctl daemon-reload
 sudo systemctl enable scr.service
